75d5a3428fec4396dc85582de2685495041c5da9
STEPHANO MATHEUS <stephano.matheus@senacsp.edu.br>
2022-04-20 14:05:55 -0300

    first commit

---
+++ Exercicio  TI14T/ModelExercicios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio__TI14T
+{
+    class ModelExercicios
+    {
+        private int num1;
+        private int num2;
+
+        public ModelExercicios()
+
+    }
+}
---
+++ Exercicio  TI14T/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio__TI14T
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }//Fim do metodo Main
+    }// fim da Classe
+}//fim do projeto
